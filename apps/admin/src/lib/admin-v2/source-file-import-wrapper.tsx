2ed6984620d9621b4402c750676d36688bd92f8e
yoomarks <lukeluccas6292643@gmail.com>
2026-08-13 23:14:26 +0800

    chore: remove unused file import wrapper

--- apps/admin/src/lib/admin-v2/source-file-import-wrapper.tsx
+++
@@ -1,5 +0,0 @@
-import { SourceFileImport } from "@/components/sources/source-file-import";
-
-export function SourceFileImportWrapper({ workspaceId }: { workspaceId: string }) {
-  return <SourceFileImport workspaceId={workspaceId} />;
-}
