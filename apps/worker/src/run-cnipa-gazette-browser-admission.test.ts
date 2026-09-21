3c278c51d882def3f085a8b7b490c389b689a3b9
yoomarks <lukeluccas6292643@gmail.com>
2026-09-21 22:48:43 +0800

    test(cnipa): update runner admission fixture

--- apps/worker/src/run-cnipa-gazette-browser-admission.test.ts
+++ apps/worker/src/run-cnipa-gazette-browser-admission.test.ts
@@ -44,12 +44,19 @@ const plan = parseCnipaGazetteBrowserAdmissionPlan({
     sha256: "a".repeat(64),
     sizeBytes: 735,
   },
-  chunkRequestRef: {
-    artifactId: "art_01ARZ3NDEKTSV4RRFFQ69G5FAW",
-    canonicalUri: `cnipa://trademark-gazette/issue/75/dataset/${datasetSha}/fact-admission/chunk/1-13/request`,
-    sha256: "b".repeat(64),
-    sizeBytes: 262104,
-  },
+  chunkRequests: [
+    {
+      range: { startPage: 1, endPage: 13 },
+      requestRef: {
+        artifactId: "art_01ARZ3NDEKTSV4RRFFQ69G5FAW",
+        canonicalUri:
+          `cnipa://trademark-gazette/issue/429/dataset/${datasetSha}` +
+          "/fact-admission/chunk/1-13/request",
+        sha256: "b".repeat(64),
+        sizeBytes: 262104,
+      },
+    },
+  ],
   dataEngineUrl: "http://127.0.0.1:8081",
   historicalReplayActivated: false,
 });
